e79fa8c869d744f4a64266923c1f5d07c4ea249e
nirvan7 <75612989+nirvan7@users.noreply.github.com>
2021-02-21 18:03:50 +0530

    Add files via upload

---
+++ js/Monster.js
@@ -0,0 +1,32 @@
+class Monster {
+    constructor(x,y,r)
+    {
+      var options= {
+        isStatic: true,
+        density: 1,
+        FrictionAir: 1
+     }
+       this.x=x;
+       this.y=y;
+       this.r=r;
+     this.image=loadImage("images/Monster-01.png");
+     this.body=Bodies.circle(this.x,this.y,(this.r)/2,options);
+     World.add(world,this.body);
+    }
+    display()
+    {
+      var Pos=this.body.position;	
+      push();
+      translate(Pos.x,Pos.y);
+      scale(3)
+      rectMode(CENTER);
+      rotate(this.body.angle)
+      fill(255,0,255)
+      imageMode(CENTER);
+      ellipseMode(CENTER);
+      image(this.image, 0,0,this.r*2, this.r*2);
+      pop();
+    }
+    }
+    
+    
